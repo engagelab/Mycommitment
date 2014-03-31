271963ac82c9b73ea4e58dfb7649c75bde5caf7b
Jeremy Toussaint <jeremyt@uio.im>
2014-03-31 13:14:41 +0200

    image CRUD operations

--- web.rb
+++ web.rb
@@ -1,6 +1,6 @@
 require 'sinatra'
 require 'mongoid'
-require 'digest'
+#require 'digest'
 require 'uri'
 require 'json'
 require 'fileutils'
@@ -11,7 +11,6 @@ require 'bcrypt'
 
 
 class Gamification < Sinatra::Application
-  include BCrypt
 
   set :environment, :production
   set :public_folder, 'public'
@@ -34,4 +33,84 @@ class Gamification < Sinatra::Application
   get '/' do
     'Hello mycommitment'
   end
+
+  ######################## User ##################################
+  ### list all storage
+  get '/image' do
+    content_type :json
+    @storage = Storage.all()
+    return @storage.to_json
+  end
+
+  get '/image/:image_id' do
+    content_type :json
+    storage = Storage.find(params[:image_id])
+    storage.to_json
+  end
+
+  ## post image
+  post '/image' do
+    awskey     = ENV['AWS_ACCESS_KEY_ID']
+    awssecret  = ENV['AWS_SECRET_ACCESS_KEY']
+    bucket     = 'net.engagelab.mycommitment'
+    file       = params[:file][:tempfile]
+    filename   = params[:file][:filename]
+    filextension = filename.split('.').last
+    imageuid   = SecureRandom.uuid+'.'+filextension
+
+    AWS::S3::DEFAULT_HOST.replace 's3-us-west-2.amazonaws.com'
+    AWS::S3::Base.establish_connection!(
+        :access_key_id     => awskey,
+        :secret_access_key => awssecret
+    )
+
+    AWS::S3::S3Object.store(
+        imageuid,
+        open(file.path),
+        bucket,
+        :access => :public_read
+    )
+
+    if AWS::S3::Service.response.success?
+      ui = Storage.create(:name => filename, :url => "http://#{bucket}.s3.amazonaws.com/#{imageuid}", :s3id => imageuid)
+      return ui.to_json
+    else
+      error 404
+    end
+  end
+
+  ### delete image
+  delete '/image/:image_id' do
+    request.body.rewind  # in case someone already read it
+    content_type :json
+
+    media = Storage.find(params[:image_id])
+
+    if media.nil? then
+      status 404
+    else
+      awskey     = ENV['AWS_ACCESS_KEY_ID']
+      awssecret  = ENV['AWS_SECRET_ACCESS_KEY']
+      bucket     = 'net.engagelab.mycommitment'
+
+      AWS::S3::DEFAULT_HOST.replace 's3-us-west-2.amazonaws.com'
+      AWS::S3::Base.establish_connection!(
+          :access_key_id     => awskey,
+          :secret_access_key => awssecret
+      )
+
+      s3media = AWS::S3::S3Object.delete media.s3id, bucket
+
+      if s3media then
+        if media.destroy then
+          status 200
+          return {"message" => "Image deleted"}.to_json
+        else
+          status 500
+        end
+      end
+    end
+  end
 end
+
+require_relative 'models/init'
